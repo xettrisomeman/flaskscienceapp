c1b15685545e5fd401425d1a595787458cac93af
webizfabulous <webizmoxx@gmail.com>
2019-08-14 21:05:20 +0545

    Added templates and datas

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 from app import app
-
-
+from .models import Scientist , Theory
+from flask import render_template
 
 
 @app.route('/')
 def index():
     return 'Hello'
+
+@app.route('/scientist')
+def scientist():
+    queries = Scientist.query.all()
+    return render_template('data.htm' , datas=queries)
+
 
